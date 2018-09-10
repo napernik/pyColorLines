f82b4aa3e461987b91df101fd4555a20594a3703
Dmitry Dzygin <dmitriy.dzygin@gmail.com>
2018-09-10 22:01:21 +0200

    Refactoring in preparation to making window resizable

--- graphics.py
+++ graphics.py
@@ -3,26 +3,10 @@ from gameconfig import *
 
 screen = None
 
-square_side = 38
-ball_radius = 13
+emptySquare = pygame.image.load("square.png")
+default_tile_width = emptySquare.get_width()
 
-ball_diagonal_offset = -2
-shadow_animation_movement_range = 5
-
-pending_ball_image_size = 20
-pending_ball_image_offset = (square_side - pending_ball_image_size) / 2 - ball_diagonal_offset
-
-# Grid and screen sizes
-grid_left = 30
-grid_top = 50
-
-grid_right = grid_left + square_side * TILES_COUNT
-grid_bottom = grid_top + square_side * TILES_COUNT
-
-grid_right_margin = 30
-grid_bottom_margin = 30
-
-screen_size = screen_width, screen_height = grid_right + grid_right_margin, grid_bottom + grid_bottom_margin
+BACKGROUND_COLOR = 200, 200, 200
 
 # Score text
 pygame.font.init()
@@ -30,26 +14,56 @@ scoreFont = pygame.font.SysFont('Comic Sans MS', 30)
 scoreColor = (43, 91, 132)
 scoreCoords = (30, 5)
 
+# Grid and screen sizes
+grid_left = 30
+grid_top = 50
+
+grid_right_margin = 30
+grid_bottom_margin = 30
+
+class Proportions:
+    def __init__(self, tile_size):
+        self.ball_radius = tile_size // 3
+        self.tile_size = tile_size
+
+        self.ball_diagonal_offset = - tile_size // 15
+        self.shadow_animation_movement_range = tile_size // 7
+
+        self.pending_ball_image_size = tile_size // 2
+        self.pending_ball_image_offset = (tile_size - self.pending_ball_image_size) // 2 - self.ball_diagonal_offset
+
+        self.grid_right = grid_left + tile_size * TILES_COUNT
+        self.grid_bottom = grid_top + tile_size * TILES_COUNT
+
+        self.screen_size = self.grid_right + grid_right_margin, self.grid_bottom + grid_bottom_margin
+
+    def tile_coordinates(self, col, row):
+        "calculates the the coordinates of the top left corner of a grid tile"
+
+        return grid_left + self.tile_size * col, grid_top + self.tile_size * row
+
+proportions = Proportions(default_tile_width)
+
+
+
+
 def init():
     global screen
-    screen = pygame.display.set_mode(screen_size, pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
+    screen = pygame.display.set_mode(proportions.screen_size, pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.RESIZABLE)
 
-
-background_color = 200, 200, 200
-
-emptySquare = pygame.image.load("square.png")
 
 def add_diagonal_offset(coords, value):
     return (coords[0] + value, coords[1] + value)
 
-def square_coordinates(col, row):
-    return grid_left + square_side * col, grid_top + square_side * row
 
-def generate_ball_image(red, green, blue):
-    image = pygame.Surface((square_side, square_side))
+
+def generate_ball_image(proportions, red, green, blue):
+    tile_size, ball_radius = proportions.tile_size, proportions.ball_radius
+
+    image = pygame.Surface((tile_size, tile_size))
     transColor = image.get_at((0,0))
     image.set_colorkey(transColor)
-    ball_top = ball_left = square_side // 2 - ball_radius + ball_diagonal_offset
+    ball_top = ball_left = tile_size // 2 - ball_radius + proportions.ball_diagonal_offset
 
     for x in range(2 * ball_radius):
         for y in range(2 * ball_radius):
@@ -60,11 +74,13 @@ def generate_ball_image(red, green, blue):
                 image.set_at((ball_left + x, ball_top + y), (brightness * red, brightness * green, brightness * blue))
     return image
 
-def generate_shadow_image():
-    image = pygame.Surface((square_side, square_side))
+def generate_shadow_image(proportions):
+    tile_size, ball_radius = proportions.tile_size, proportions.ball_radius
+
+    image = pygame.Surface((tile_size, tile_size))
     transColor = image.get_at((0,0))
     image.set_colorkey(transColor)
-    ball_top = ball_left = 1 + square_side // 2 - ball_radius
+    ball_top = ball_left = 1 + tile_size // 2 - ball_radius
 
     for x in range(2 * ball_radius):
         for y in range(2 * ball_radius):
@@ -77,20 +93,20 @@ def generate_shadow_image():
     return image 
 
 def draw_tile(col, row, ball_color, shadowOffset, isPendingBall = False):
-    coord = square_coordinates(col, row)
+    coord = proportions.tile_coordinates(col, row)
     screen.blit(emptySquare, coord)
     if ball_color > 0:
         if not isPendingBall:
             screen.blit(ball_shadow_image, add_diagonal_offset(coord, shadowOffset))
             screen.blit(ball_images[ball_color - 1], coord)
         else:
-            screen.blit(pending_ball_images[ball_color - 1], add_diagonal_offset(coord, pending_ball_image_offset))
+            screen.blit(pending_ball_images[ball_color - 1], add_diagonal_offset(coord, proportions.pending_ball_image_offset))
 
-    return (coord[0], coord[1], square_side, square_side)
+    return (coord[0], coord[1], proportions.tile_size, proportions.tile_size)
 
 
 def draw_scene(gameState):
-    screen.fill(background_color)
+    screen.fill(BACKGROUND_COLOR)
     for col in range(TILES_COUNT):
         for row in range(TILES_COUNT):
             ball_color = gameState.balls[col][row]
@@ -106,32 +122,27 @@ def draw_scene(gameState):
 
 def animateSelectedBall(balls, coords, timeCounter):
     color = balls[coords[0]][coords[1]]
-    shadowOffset = ball_diagonal_offset + shadow_animation_movement_range * (1 + math.sin(timeCounter / 2.5)) / 2
+    shadowOffset = proportions.ball_diagonal_offset + proportions.shadow_animation_movement_range * (1 + math.sin(timeCounter / 2.5)) / 2
     updateArea = draw_tile(coords[0], coords[1], color, shadowOffset)
     pygame.display.update(updateArea)
     
 
 
 def check_board_click(x, y, onCellClick):
-    if x >= grid_left and x < grid_right and y >= grid_top and y < grid_bottom:
-        col = (x - grid_left) // square_side
-        row = (y - grid_top) // square_side
+    if x >= grid_left and x < proportions.grid_right and y >= grid_top and y < proportions.grid_bottom:
+        col = (x - grid_left) // proportions.tile_size
+        row = (y - grid_top) // proportions.tile_size
         onCellClick(col, row)
 
-ball_images = (
-    generate_ball_image(255, 0, 0), 
-    generate_ball_image(0, 255, 0),
-    generate_ball_image(0, 0, 255),
-    generate_ball_image(255, 255, 0),
-    generate_ball_image(0, 255, 255),
-    generate_ball_image(255, 0, 255)
-)
+ball_images = [generate_ball_image(proportions, *color) for color in BALL_COLORS]
 
 pending_ball_images = []
 
+
+
 for imageIndex in range(len(ball_images)):
-    newSize = (pending_ball_image_size, pending_ball_image_size)
+    newSize = (proportions.pending_ball_image_size, proportions.pending_ball_image_size)
     #pending_ball_images.append(pygame.transform.smoothscale(ball_images[imageIndex], newSize))
     pending_ball_images.append(pygame.transform.scale(ball_images[imageIndex], newSize))
 
-ball_shadow_image = generate_shadow_image()
+ball_shadow_image = generate_shadow_image(proportions)
